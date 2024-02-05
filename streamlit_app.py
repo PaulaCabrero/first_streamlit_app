736d8da65baeee3fce874c9428b816fc68cddada
PaulaCabrero <159008300+PaulaCabrero@users.noreply.github.com>
2024-02-05 11:05:36 +0100

    Update streamlit_app.py
    new title, logos, header and avocado text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
-streamlit.title('My parents new healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & rocket smoothie')
-streamlit.text('Hard-boiled Free-Range Egg')
+streamlit.title('My Mom's new healthy Diner')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')  
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & rocket smoothie')
+streamlit.text('🐔 Hard-boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
