5781edc86199e7060b84ae64bea9adb7c191cb02
PaulaCabrero <159008300+PaulaCabrero@users.noreply.github.com>
2024-02-05 11:06:35 +0100

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('My Mom's new healthy Diner')
+streamlit.title('My Mom new healthy Diner')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')  
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & rocket smoothie')
