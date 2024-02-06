ad424496485cb26f467ec68b65d313c8d6c3098e
PaulaCabrero <159008300+PaulaCabrero@users.noreply.github.com>
2024-02-06 10:39:17 +0100

    Update streamlit_app.py
    cositas nuevas de código

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,19 +23,22 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+# nueva parte para la respueta de la api 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-#import requests   se ha movido arriba
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response.json()) # importa el json
-
-# coge el fichero json lo carga y normaliza
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# mete los datos anteriores en una tabla
-streamlit.dataframe(fruityvice_normalized)
-
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error('Please select a fruit to get information.')
+  else
+    #import requests   se ha movido arriba
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    #streamlit.text(fruityvice_response.json()) # importa el json
+    # coge el fichero json lo carga y normaliza
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # mete los datos anteriores en una tabla
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 # no ejecute  nada a partir de aquí hasta que lo arreglemos
 streamlit.stop()
 
